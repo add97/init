a10a60a5528dccb280e734e6d2095edb19282df0
Alexander Du <Alexander Du>
2017-04-11 23:11:53 -0400

    ''

--- client/application/header/header.js
+++ client/application/header/header.js
@@ -1,6 +1,9 @@
 Template.navigation.helpers({
   routeName(){
     return !Router.current() ? false : Router.current().options.route.getName();
+  },
+  checkRoute(routeName){
+
   }
   // search(){
   //
